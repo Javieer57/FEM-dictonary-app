052714cfde51f37937cf2643bc7f56f37b08f68b
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-03-14 14:05:17 -0600

    feat: config results meanings

--- src/js/showResults.js
+++ src/js/showResults.js
@@ -1,100 +1,85 @@
 import data from "./API.json";
 
 const resultsContainer = document.getElementById("results");
-const headerTemplate = document.getElementById("results-header").content;
+const headerTemplate = document
+  .getElementById("results-header")
+  .content.cloneNode(true);
 const meaningsTemplate = document.getElementById("results-meanings").content;
 
-const headerDOM = {
-  title: headerTemplate.querySelector(".results__title"),
-  pronunciation: headerTemplate.querySelector(".results__pronunciation-label"),
-  audioBtn: headerTemplate.querySelector(".results__play-button"),
-};
-
 export const showResults = (data) => {
-  const header = setHeader(data);
+  const header = getHeader(data);
+  const meanings = getMeanings(data);
+
   resultsContainer.appendChild(header);
+  meanings.forEach((meaning) => {
+    resultsContainer.appendChild(meaning);
+  });
 };
 
-const setHeader = (data) => {
-  const { word, phonetic, audio } = getFilteredData(data);
-  const { title, pronunciation, audioBtn } = headerDOM;
+const getHeader = (data) => {
+  const { word, phonetics } = data[0];
+  const phonetic = phonetics[phonetics.length - 1].text;
+  const audio = phonetics[phonetics.length - 1].audio;
+
+  const clone = headerTemplate.cloneNode(true);
+  const title = clone.getElementById("title");
+  const pronunciation = clone.getElementById("IPA");
+  const audioBtn = clone.getElementById("button");
 
   title.textContent = word;
   pronunciation.textContent = phonetic;
-  audioBtn.setAttribute("onclick", `new Audio('${audio}').play();`);
+  audioBtn.setAttribute("onclick", 'new Audio("' + audio + '").play();');
 
-  return headerTemplate.cloneNode(true);
+  return clone;
 };
 
-// const showResults = (data) => {
-//   const {
-//     title,
-//     results,
-//     pronunciation,
-//     audioBtn,
-//     details,
-//     partOfSpeech,
-//     meaningList,
-//     meaningListItem,
-//   } = getTemplateSeccions();
-//   const { word, phonetic, audio, meanings } = getFilteredData(data);
-
-//   title.textContent = word;
-//   pronunciation.textContent = phonetic;
-//   audioBtn.setAttribute("onclick", `new Audio('${audio}').play();`);
-
-//   meanings.forEach((meaning) => {
-//     partOfSpeech.textContent = meaning[0].partOfSpeech;
-
-//     meaningList.innerHTML = "";
-//     meaning[0].definitions.forEach(({ definition }) => {
-//       meaningListItem.textContent = definition;
-//       meaningList.appendChild(meaningListItem.cloneNode(true));
-//     });
-
-//     const clone = details.cloneNode(true);
-//     template.appendChild(clone);
-//   });
-
-//   const clone = template.cloneNode(true);
-//   fragment.appendChild(clone);
-//   dictionaryContainer.appendChild(fragment);
-// };
-
-// const getTemplateSeccions = () => {
-//   const results = template.querySelector(".results");
-//   const title = results.querySelector(".results__title");
-//   const pronunciation = results.querySelector(".results__pronunciation-label");
-//   const audioBtn = results.querySelector(".results__play-button");
-//   const details = results.querySelector(".results__details");
-//   const partOfSpeech = details.querySelector(".results__part-of-speech-value");
-//   const meaningList = details.querySelector(".results__list--meaning");
-//   const meaningListItem = meaningList.querySelector(".results__item");
-
-//   return {
-//     results,
-//     title,
-//     pronunciation,
-//     audioBtn,
-//     details,
-//     partOfSpeech,
-//     meaningList,
-//     meaningListItem,
-//   };
-// };
-
-const getFilteredData = (data) => {
-  const word = data[0].word;
-  const phonetic = data[0].phonetics[1].text;
-  const audio = data[0].phonetics[data[0].phonetics.length - 1].audio;
+const getMeanings = (data) => {
   const meanings = data.map(({ meanings }) => meanings);
 
-  return {
-    word,
-    phonetic,
-    audio,
-    meanings,
-  };
+  const meaningListItem = meaningsTemplate
+    .querySelector(".results__item")
+    .cloneNode(true);
+
+  const fragmentsArray = meanings.map((meaning) => {
+    const clone = meaningsTemplate.cloneNode(true);
+    const partOfSpeech = clone.querySelector(".results__part-of-speech-value");
+    const meaningList = clone.querySelector(".results__list--meaning");
+
+    partOfSpeech.textContent = meaning[0].partOfSpeech;
+    meaningList.textContent = "";
+
+    meaning[0].definitions.forEach(({ definition }) => {
+      meaningListItem.textContent = definition;
+      meaningList.appendChild(meaningListItem.cloneNode(true));
+    });
+
+    const resultsDetails = clone.querySelector(".results__details");
+    const alternativesList = resultsDetails.querySelector(
+      ".results__alternatives"
+    );
+    const alternativeTitle = alternativesList.querySelector("h3");
+    const alternatives = alternativesList.querySelector("p");
+    const synonyms = meaning[0].synonyms;
+    const antonyms = meaning[0].antonyms;
+
+    alternativesList.remove();
+
+    if (synonyms.length > 0) {
+      alternativeTitle.textContent = "Synonyms:";
+      alternatives.textContent = synonyms.join(", ");
+      resultsDetails.appendChild(alternativesList.cloneNode(true));
+    }
+
+    if (antonyms.length > 0) {
+      alternativeTitle.textContent = "Antonyms:";
+      alternatives.textContent = antonyms.join(", ");
+      resultsDetails.appendChild(alternativesList.cloneNode(true));
+    }
+
+    return clone;
+  });
+
+  return fragmentsArray;
 };
 
 showResults(data);
