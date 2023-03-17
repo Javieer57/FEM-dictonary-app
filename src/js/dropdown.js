8b7c9b6d0687d268992d4800e3f328aa33a5d9fa
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-03-17 17:55:16 -0600

    feat: add font selector functionality

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -2,10 +2,6 @@ const dropdown = document.querySelector(".dropdown");
 const button = dropdown.querySelector(".dropdown__button");
 const menu = dropdown.querySelector(".dropdown__menu");
 
-button.addEventListener("click", toggleDropdown);
-button.addEventListener("keydown", handleButtonKeydown);
-menu.addEventListener("keydown", handleMenuKeydown);
-
 function toggleDropdown() {
   const isOpen = dropdown.classList.toggle("dropdown--open");
   dropdown.setAttribute("aria-expanded", isOpen.toString());
@@ -13,19 +9,29 @@ function toggleDropdown() {
 }
 
 function handleButtonKeydown(event) {
-  if (event.code === "Space" || event.code === "Enter") {
+  const { code } = event;
+  if (code === "Space" || code === "Enter") {
     event.preventDefault();
     toggleDropdown();
-  } else if (event.code === "Escape") {
-    dropdown.classList.remove("dropdown--open");
-    dropdown.setAttribute("aria-expanded", "false");
-    menu.setAttribute("aria-hidden", "true");
+  } else if (code === "Escape") {
+    closeDropdown();
   }
 }
 
 function handleMenuKeydown(event) {
-  if (event.code === "Escape") {
-    dropdown.classList.remove("dropdown--open");
-    button.focus();
+  const { code } = event;
+  if (code === "Escape") {
+    closeDropdown();
   }
 }
+
+function closeDropdown() {
+  dropdown.classList.remove("dropdown--open");
+  dropdown.setAttribute("aria-expanded", "false");
+  menu.setAttribute("aria-hidden", "true");
+  button.focus();
+}
+
+button.addEventListener("click", toggleDropdown);
+button.addEventListener("keydown", handleButtonKeydown);
+menu.addEventListener("keydown", handleMenuKeydown);
