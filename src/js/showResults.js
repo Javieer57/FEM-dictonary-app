3d0a214d4aef1fa7232b71a5fadc24911d40207b
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-03-20 19:34:30 -0600

    feat: change html templates for template literals

--- src/js/showResults.js
+++ src/js/showResults.js
@@ -1,8 +1,4 @@
 const resultsContainer = document.getElementById("results");
-const headerTemplate = document.getElementById("results-header").content;
-const meaningsTemplate = document.getElementById("results-meanings").content;
-const sourceTemplate = document.getElementById("results-source").content;
-const notFoundTemplate = document.getElementById("results-not-found").content;
 const loadingSpinner = document.getElementById("spinner-wrapper");
 
 export const showSpinner = () => {
@@ -20,95 +16,103 @@ export const clearResults = () => {
 
 export const showResults = (data) => {
   clearResults();
-  const header = getHeader(data);
-  const meanings = getMeanings(data);
-  const source = getSource(data);
-
-  resultsContainer.appendChild(header);
-
-  meanings.forEach((meaning) => {
-    resultsContainer.appendChild(meaning);
-  });
-
-  resultsContainer.appendChild(source);
+  let results = ``;
+  results += generateHeader(data);
+  results += generateDefinitions(data);
+  results += generateSource(data);
+  resultsContainer.innerHTML = results;
 };
 
-const getSource = (data) => {
-  const source = sourceTemplate.cloneNode(true);
-  const sourceLink = source.querySelector("a");
-  sourceLink.href = data[0].sourceUrls[0];
-  sourceLink.textContent = sourceLink.href;
+function generateSource(data) {
+  const link = data[0].sourceUrls[0];
+
+  const source = `
+    <section class="results__source">
+      <span class="separator-line mb-20"></span>
+      <p class="results__source-label fs-sm">
+        <strong class="fw-400 secondary-text mr-20 underline">Source</strong>
+        <a class="mr-10" href="${link}" target="_blank" rel="noopener noreferrer">${link}</a>
+        <i class="new-window-icon"></i>
+      </p>
+    </section>
+  `;
+
   return source;
-};
+}
 
-const getHeader = (data) => {
+function generateHeader(data) {
   const { word, phonetics } = data[0];
   const phonetic = phonetics[phonetics.length - 1].text;
   const audio = phonetics[phonetics.length - 1].audio;
 
-  const clone = headerTemplate.cloneNode(true);
-  const title = clone.getElementById("title");
-  const pronunciation = clone.getElementById("IPA");
-  const audioBtn = clone.getElementById("button");
+  const header = `
+    <header class="mb-40 d-flex align-items-center justify-content-between">
+      <div>
+        <h2 class="fs-xxl mb-10" id="title">${word}</h2>
+        <p class="fs-xl mb-0 contrast-text">
+            <span class="sr-only">Pronunciation:</span>
+            ${phonetic}
+        </p>
+      </div>
+      <div>
+        <button class="results__play-button" id="button" aria-label="Play pronunciation" onclick="new Audio('${audio}').play();"></button>
+      </div>
+    </header>
+  `;
 
-  title.textContent = word;
-  pronunciation.textContent = phonetic;
-  audioBtn.setAttribute("onclick", 'new Audio("' + audio + '").play();');
+  return header;
+}
 
-  return clone;
-};
+function generateDefinitions(data) {
+  const meanings = data[0].meanings;
 
-const getMeanings = (data) => {
-  const meanings = data.map(({ meanings }) => meanings);
-
-  const meaningListItem = meaningsTemplate
-    .querySelector(".results__item")
-    .cloneNode(true);
-
-  const fragmentsArray = meanings.map((meaning) => {
-    const clone = meaningsTemplate.cloneNode(true);
-    const partOfSpeech = clone.querySelector(".results__part-of-speech-value");
-    const meaningList = clone.querySelector(".results__list--meaning");
-
-    partOfSpeech.textContent = meaning[0].partOfSpeech;
-    meaningList.textContent = "";
-
-    meaning[0].definitions.forEach(({ definition }) => {
-      meaningListItem.textContent = definition;
-      meaningList.appendChild(meaningListItem.cloneNode(true));
-    });
-
-    const resultsDetails = clone.querySelector(".results__details");
-    const alternativesList = resultsDetails.querySelector(
-      ".results__alternatives"
+  const generateDefinitions = (definitions) =>
+    definitions.map(
+      ({ definition }) =>
+        `<li class="results__item p-left-20 mb-15 fs-md">${definition}</li>`
     );
-    const alternativeTitle = alternativesList.querySelector("h3");
-    const alternatives = alternativesList.querySelector("p");
-    const synonyms = meaning[0].synonyms;
-    const antonyms = meaning[0].antonyms;
 
-    alternativesList.remove();
+  const generateAlternatives = (type, alternatives) => {
+    if (alternatives.length === 0) return "";
 
-    if (synonyms.length > 0) {
-      alternativeTitle.textContent = "Synonyms:";
-      alternatives.textContent = synonyms.join(", ");
-      resultsDetails.appendChild(alternativesList.cloneNode(true));
-    }
+    return `
+      <div class="results__alternatives mb-40 d-flex align-items-start gap-24">
+        <h3 class="secondary-text fw-400 fs-lg mb-0">${type}:</h3>
+        <p class="contrast-text fw-700 fs-lg mb-0">
+          ${alternatives.join(", ")}
+        </p>
+      </div>
+    `;
+  };
 
-    if (antonyms.length > 0) {
-      alternativeTitle.textContent = "Antonyms:";
-      alternatives.textContent = antonyms.join(", ");
-      resultsDetails.appendChild(alternativesList.cloneNode(true));
-    }
+  const generateMeaningPart = (meaning) => `
+    <section class="results__details mb-40">
+      <p class="d-flex align-items-center gap-24 font-xl italic fw-700 mb-40">
+        <strong class="sr-only">Part of speech:</strong>
+        <span class="results__part-of-speech-value fs-xl">
+          ${meaning.partOfSpeech}
+        </span>
+        <span class="separator-line"></span>
+      </p>
+      <h3 class="secondary-text fw-400 fs-lg mb-40">Meaning:</h3>
+      <ul class="results__list--meaning mb-40">
+        ${generateDefinitions(meaning.definitions).join("")}
+      </ul>
+      ${generateAlternatives("Synonyms", meaning.synonyms)}
+      ${generateAlternatives("Antonyms", meaning.antonyms)}
+    </section>
+  `;
 
-    return clone;
-  });
-
-  return fragmentsArray;
-};
+  return meanings.map(generateMeaningPart).join("");
+}
 
 export const showNotFound = () => {
   clearResults();
-  const clone = notFoundTemplate.cloneNode(true);
-  resultsContainer.appendChild(clone);
+  resultsContainer.innerHTML = `
+    <section class="results__not-found ta-center mt-130">
+      <span class="d-inline-block fs-xxl mb-45">🤔</span>
+      <h3 class="fs-lg mb-25">No Definitions Found</h3>
+      <p class="fs-md secondary-text">Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
+    </section>
+  `;
 };
